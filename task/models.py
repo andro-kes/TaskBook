53554f3cb7106a6e205f842bf1e34bab865cd7b8
Andrey Denisov <and.denisov26@gmail.com>
2024-03-17 22:05:17 +0300

    Модели для Задач и Тегов

--- task/models.py
+++ task/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class TagModel(models.Model):
+    color_list = (
+        ('красный', 'красный'),
+        ('синий', 'синий'),
+        ('желтый', 'желтый'),
+        ('зеленый', 'зеленый'),
+    )
+    
+    tag_name = models.CharField(max_length=50)
+    color = models.CharField(max_length=10, choices=color_list)
+
+class TaskModel(models.Model):
+    category_list = (
+        ('очень важно', 'очень важно'),
+        ('важно', 'важно'),
+        ('желательно', 'желательно'),
+        ('мечтаю', 'мечтаю'),
+    )
+    
+    task_name = models.CharField(max_length=250)
+    category = models.CharField(max_length=20, choices=category_list)
+    tag = models.ForeignKey(TagModel, on_delete=models.CASCADE)
