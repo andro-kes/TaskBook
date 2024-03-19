88606e1de31eb7360fe695b69976ddd704e5a301
Andrey Denisov <and.denisov26@gmail.com>
2024-03-19 21:43:38 +0300

    вьюшка для показа всех задач

--- task/views.py
+++ task/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import TaskModel
 
-# Create your views here.
+class ListTaskAPIView(ListAPIView):
+    model = TaskModel
+    queryset = TaskModel.objects.all()
