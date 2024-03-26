6277e09ada7e902450fc365ea78dbb73dc9bd456
Andrey Denisov <and.denisov26@gmail.com>
2024-03-26 23:42:18 +0300

    Фильтрация

--- task/views.py
+++ task/views.py
@@ -1,9 +1,14 @@
 from rest_framework.generics import ListAPIView
 from .serializers import TaskSerializer
 from .models import TaskModel
+from rest_framework import filters
 
 class ListTaskAPIView(ListAPIView):
     queryset = TaskModel.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['task_name']
+    ordering_fields = ['task_name']
+    
     
 
